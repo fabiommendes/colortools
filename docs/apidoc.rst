53f09cd4cd56ba4ec6363f60d47c885bf4113982
fabiomacedomendes <fabiomacedomendes@gmail.com>
2016-09-08 21:18:31 -0300

    initial commit

---
+++ docs/apidoc.rst
@@ -0,0 +1,8 @@
+=============
+API Reference
+=============
+
+API documentation for the colortools module.
+
+.. automodule:: colortools
+   :members:
---
+++ src/colortools/tests/test_colortools.py
@@ -0,0 +1,47 @@
+import pytest
+from colortools import Color
+
+
+def test_init_rgb_color_correctly():
+    c = Color(1, 2, 3)
+    assert c.red == 1
+    assert c.green == 2
+    assert c.blue == 3
+    assert c.alpha == 255
+
+
+def test_init_from_rgba():
+    assert Color(1, 2, 3, 255) == Color(1, 2, 3)
+
+
+def test_init_color_from_name():
+    assert Color('red') == Color(255, 0, 0)
+    assert Color('lime') == Color(0, 255, 0)  # HTML name convention...
+    assert Color('blue') == Color(0, 0, 255)
+
+
+def test_init_from_3_hex():
+    assert Color('#0F0') == Color(0, 255, 0)
+
+
+def test_init_from_4_hex():
+    assert Color('#0F01') == Color(0, 255, 0, 17)
+
+
+def test_init_from_6_hex():
+    assert Color('#00FF00') == Color(0, 255, 0)
+
+
+def test_init_from_8_hex():
+    assert Color('#00FF0080') == Color(0, 255, 0, 128)
+
+
+def test_color_compares_with_tuple():
+    color = Color('red')
+    assert color == (255, 0, 0, 255)
+    assert color == (255, 0, 0)
+
+
+def test_init_color_from_color():
+    black = Color('black')
+    assert Color(black) == black
